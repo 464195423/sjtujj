68efe9bf102a8ace3d22699654f97e3ef24b82ac
xiaozzz <philip.20050132@163.com>
2015-03-09 20:44:36 +0800

    修改1

---
+++ 滑动tab/T1Activity.java
@@ -0,0 +1,15 @@
+package com.example.sjtujj;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class T1Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_t1);
+	}
+}
