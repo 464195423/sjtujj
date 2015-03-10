e3ffa72152f659bfe08e13ef24ab10b0a4b1d629
xiaozzz <philip.20050132@163.com>
2015-03-10 20:20:02 +0800

    a2

--- src/com/example/sjtujj/T4Activity.java
+++ src/com/example/sjtujj/T4Activity.java
@@ -1,15 +1,43 @@
 package com.example.sjtujj;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class T4Activity extends Activity {
-
+private ImageView iv;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_t4);
+		
+		iv = (ImageView)findViewById(R.id.iv2);
+		iv.setOnClickListener(new View.OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				AlertDialog alert = new AlertDialog.Builder(T4Activity.this).create();
+				alert.setTitle("提示消息");
+				alert.setMessage("功能开发中");
+				alert.setButton(DialogInterface.BUTTON_POSITIVE,"确定", new OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+
+					}
+				});
+				alert.show();
+			}
+			
+		});
 	}
 }
