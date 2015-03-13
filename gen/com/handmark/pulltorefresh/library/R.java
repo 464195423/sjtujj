3f7616654cbedb055f943db4555cdbe3d1ceb335
xiaozzz <philip.20050132@163.com>
2015-03-13 18:19:55 +0800

    3.13

--- gen/com/handmark/pulltorefresh/library/R.java
+++ gen/com/handmark/pulltorefresh/library/R.java
@@ -51,7 +51,7 @@ public final class R {
 	public static final class id {
 		public static final int both = 0x7f06000c;
 		public static final int disabled = 0x7f06000d;
-		public static final int fl_inner = 0x7f06007e;
+		public static final int fl_inner = 0x7f060081;
 		public static final int flip = 0x7f060013;
 		public static final int gridview = 0x7f060000;
 		public static final int manualOnly = 0x7f06000e;
@@ -59,10 +59,10 @@ public final class R {
 		public static final int pullFromEnd = 0x7f060010;
 		public static final int pullFromStart = 0x7f060011;
 		public static final int pullUpFromBottom = 0x7f060012;
-		public static final int pull_to_refresh_image = 0x7f06007f;
-		public static final int pull_to_refresh_progress = 0x7f060080;
-		public static final int pull_to_refresh_sub_text = 0x7f060082;
-		public static final int pull_to_refresh_text = 0x7f060081;
+		public static final int pull_to_refresh_image = 0x7f060082;
+		public static final int pull_to_refresh_progress = 0x7f060083;
+		public static final int pull_to_refresh_sub_text = 0x7f060085;
+		public static final int pull_to_refresh_text = 0x7f060084;
 		public static final int rotate = 0x7f060014;
 		public static final int scrollview = 0x7f060002;
 		public static final int webview = 0x7f060001;
--- src/com/example/sjtujj/T2Activity.java
+++ src/com/example/sjtujj/T2Activity.java
@@ -1,21 +1,34 @@
 package com.example.sjtujj;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener2;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class T2Activity extends Activity {
+public class T2Activity extends ListActivity implements OnRefreshListener2<ListView>{
 private TextView tv1;
 private TextView tv2;
 private TextView tv3;
 private int type = 1;
+private PullToRefreshListView lv; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -82,7 +95,55 @@ private int type = 1;
 			}		
 		});
 		
+		lv = (PullToRefreshListView)findViewById(R.id.t2_lv);  
+		  
+        lv.setMode(Mode.PULL_DOWN_TO_REFRESH);
+		lv.getLoadingLayoutProxy(true, false).setPullLabel("下拉刷新");
+		lv.getLoadingLayoutProxy(true, false).setReleaseLabel("放开以刷新");
+		lv.getLoadingLayoutProxy(true, false).setRefreshingLabel("正在刷新...");
+		lv.setOnRefreshListener(this);
+		lv.setDividerPadding(10);
+		lv.getRefreshableView().setDividerHeight(0);
 		
+		lv.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				
+			}
+
+		});
+		
+		
+		List<String> list = new ArrayList<String>();  
+        list.add("loonggg");  
+        list.add("我们都是开发者");  
+        list.add("我们都是开发者");  
+        list.add("我们都是开发者");  
+        list.add("我们都是开发者");  
+        list.add("我们都是开发者");  
+        list.add("我们都是开发者");  
+        list.add("我们都是开发者");  
+        list.add("我们都是开发者");  
+        list.add("我们都是开发者");  
+        list.add("我们都是开发者");  
+        LvAdapter adapter = new LvAdapter(list, this);  
+        lv.setAdapter(adapter);  
+        
+	}
+
+	@Override
+	public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
+		// TODO Auto-generated method stub
+		lv.onRefreshComplete();
+	}
+
+	@Override
+	public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
+		// TODO Auto-generated method stub
+		lv.onRefreshComplete();
 		
 	}
 }
